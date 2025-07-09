d4a671391a649261b2e334808b118b3f448a4185
faysal0x1 <faysal35-3119@diu.edu.bd>
2025-07-09 16:53:21 +0600

    Cors policy issue

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = require('./src/routes/api');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+}));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const app = require ('./app');
+const app = require('./app');
+const cors = require('cors');
 
 const port = 8000;
 
-app.listen (port, () => {
-    console.log (`Server is running on port ${port}`);
+app.use(cors({
+    origin: 'http://localhost:5173',
+}));
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
